8f572b09163141d4971c849d2deca2e8ba6edbbf
“plantoteachme” <amyplant@me.com>
2016-09-06 13:42:41 -0500

    Add "Save Receipt" button

--- CheckMates/DetailedReceiptTableViewController.swift
+++ CheckMates/DetailedReceiptTableViewController.swift
@@ -18,18 +18,31 @@ class DetailedReceiptTableViewController: UITableViewController {
     
     var itemStore: ItemStore!
     
+    override func viewWillAppear(animated: Bool) {
+        tableView.reloadData()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 65
+
+    }
+    
     @IBAction func addNewItem(sender: AnyObject) {
         // Create a new Item and add it to the store
-//        let newItem = itemStore.createItem()
-//        
-//        // Figure out where that item is in the array
-//        if let index = itemStore.allItems.indexOf(newItem) {
-//            let indexPath = NSIndexPath(forRow: index, inSection: 0)
-//            
-//            // Insert this new row into the table.
-//            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
-//        }
+        let newItem = itemStore.createItem("", price: 0)
+        
+        // Figure out where that item is in the array
+        if let index = itemStore.allItems.indexOf(newItem) {
+            let indexPath = NSIndexPath(forRow: index, inSection: 0)
+            
+            // Insert this new row into the table.
+            tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        }
     }
+    
     
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
@@ -55,18 +68,10 @@ class DetailedReceiptTableViewController: UITableViewController {
             presentViewController(alertController, animated: true, completion: nil)
         }
     }
-
-    override func viewWillAppear(animated: Bool) {
-        tableView.reloadData()
-    }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        tableView.rowHeight = UITableViewAutomaticDimension
-        tableView.estimatedRowHeight = 65
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
-    
     
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
