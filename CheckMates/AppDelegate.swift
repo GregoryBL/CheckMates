00136dca3a385e49b97e20ac62adffabe0bded8a
keithpilz <kpilson@gmail.com>
2016-09-06 23:45:01 -0500

    Add coredatastack class

--- CheckMates/AppDelegate.swift
+++ CheckMates/AppDelegate.swift
@@ -13,8 +13,7 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    
+    let coreDataStack = CoreDataStack(modelName: "CheckMates")
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
--- CheckMates/CoreDataStack.swift
+++ CheckMates/CoreDataStack.swift
@@ -9,6 +9,46 @@
 import Foundation
 import CoreData
 
+
+class CoreDataStack {
+    let managedObjectModelName: String
+    
+    required init(modelName: String){
+        managedObjectModelName = modelName
+    }
+    
+    private lazy var managedObjectModel: NSManagedObject = {
+        let modelURL =
+            NSBundle.mainBundle().URLForResource(self.managedObjectModelName, withExtension: "momd")!
+        return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }
+    
+    private var applicationDocumentsDirectory: NSURL = {
+        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
+        return urls.first!
+    }()
+    
+    private lazy var persistentStoreCoordinator: NSPersistentStoreCoordinator = {
+        var coordinator =
+            NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let pathComponent = "\(self.managedObjectModelName).sqlite"
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent(pathComponent)
+        
+        let store = try! coordinator.addPersistentStoreWithType(NSSQLiteStoreType,
+                                                                configuration: nil,
+                                                                URL: url,
+                                                                options: nil)
+        return coordinator
+    }()
+    
+    lazy var mainQueueContext: NSManagedObjectContext = {
+        let moc = NSManagedObjectContext(concurrencyType: .MainQueueConcurrencyType)
+        moc.persistentStoreCoordinator = self.persistentStoreCoordinator
+        moc.name = "Main Queue Context (UI Context)"
+        
+        return moc
+    }()
+}
 //lazy var applicationDocumentsDirectory: NSURL = {
 //    let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
 //    return urls[urls.count-1]
