6098c92c8008130c585829141bc1876014e67c57
keithpilz <kpilson@gmail.com>
2016-09-07 16:48:26 -0500

    Add core data and subclasses

---
+++ CheckMates/Contact+CoreDataProperties.swift
@@ -0,0 +1,25 @@
+//
+//  Contact+CoreDataProperties.swift
+//  CheckMates
+//
+//  Created by Keith Pilson on 9/7/16.
+//  Copyright © 2016 checkMates. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Contact {
+
+    @NSManaged var firstName: String?
+    @NSManaged var lastName: String?
+    @NSManaged var uuid: String?
+    @NSManaged var uiimage: NSData?
+    @NSManaged var mobileNumber: String?
+    @NSManaged var event: NSManagedObject?
+    @NSManaged var items: NSSet?
+
+}
---
+++ CheckMates/Event+CoreDataProperties.swift
@@ -0,0 +1,21 @@
+//
+//  Event+CoreDataProperties.swift
+//  CheckMates
+//
+//  Created by Keith Pilson on 9/7/16.
+//  Copyright © 2016 checkMates. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Event {
+
+    @NSManaged var createdAt: String?
+    @NSManaged var contacts: NSSet?
+    @NSManaged var receipt: Receipt?
+
+}
