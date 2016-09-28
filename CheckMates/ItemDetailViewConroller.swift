468bca7ab2e740b94a38d12887930fa96f44f073
Gregory Berns-Leone <gberns.l@gmail.com>
2016-09-28 15:46:14 -0500

    Change to use delegate and pass the added/updated item

--- CheckMates/ItemDetailViewConroller.swift
+++ CheckMates/ItemDetailViewConroller.swift
@@ -14,37 +14,36 @@ class ItemDetailViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet var priceField: UITextField!
     @IBOutlet var dateField:  UILabel!
     
-    @IBAction func doneEditing(_ sender: UIBarButtonItem) {
-        view.endEditing(true)
-        
-        if(item == nil) {
-            item = Item(title: titleField.text!, price: priceField.text!.asFloat)
-        } else {
-            item.title = titleField.text ?? ""
-            item.price = priceField.text!.asFloat
-        }
-        self.dismiss(animated:true, completion: nil)
-    }
-    
-    @IBAction func cancelEditing(_ sender: UIBarButtonItem) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    var item: Item! {
-        didSet {
-            navigationItem.title = item.title
-        }
-    }
+    var indexPath: IndexPath?
+    var item: Item?
+    var delegate: ItemDetailViewControllerDelegate?
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        if(item != nil) {
-            titleField.text = item.title
-            priceField.text = item.price.asLocaleCurrency
-            dateField.text = item.created_at.friendlyDate
+        if let existingItem = delegate?.existingItemForIndexPath(indexPath) {
+            self.item = existingItem
+            titleField.text = item!.title
+            priceField.text = item!.price.asLocaleCurrency
+            dateField.text = item!.created_at.friendlyDate
+            self.title = "Edit Item"
+        } else {
+            self.title = "New Item"
         }
-       
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        view.endEditing(true)
+        
+        var isNew = false
+        if (item == nil) {
+            item = Item(title: titleField.text!, price: priceField.text!.asFloat)
+            isNew = true
+        } else {
+            item!.title = titleField.text!
+            item!.price = priceField.text!.asFloat
+        }
+        delegate?.itemDetailViewControllerDidCompleteEditingItem(item!, new: isNew, sender: self)
     }
     
     @IBAction func backgroundTapped(_ sender: UITapGestureRecognizer) {
